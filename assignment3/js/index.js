9b9ecfe5db0533dd85c2fa79b819c9ae06ea90af
Yolandee <110145811+Yolandee@users.noreply.github.com>
2022-10-27 10:11:49 +0800

    Add files via upload

---
+++ assignment3/js/index.js
@@ -0,0 +1,11 @@
+var Num=1;
+  function rotatePhoto()
+  {
+  if(++Num>2)
+  Num=1;
+  document.getElementById("banner").src="images/banner"+Num+".jpg";
+  window.setTimeout("rotatePhoto()", 3000);
+  }
+  window.onload=function(){
+ rotatePhoto()
+}
